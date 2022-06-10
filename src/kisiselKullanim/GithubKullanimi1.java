db0ac6166f46744daffef62409923124c784af04
Fatih Turker <fthturker80@gmail.com>
2022-06-10 19:01:45 +0300

    ilk degisiklik

--- src/kisiselKullanim/GithubKullanimi1.java
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -3,5 +3,6 @@ package kisiselKullanim;
 public class GithubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("fatih turker");
     }
 }
