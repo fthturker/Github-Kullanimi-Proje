05cc555311e38ee0501ca03962d0a3d391e1a5e0
Fatih Turker <fthturker80@gmail.com>
2022-06-10 22:05:01 +0300

    10.06.2022

--- src/kisiselKullanim/GithubKullanimi1.java
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -4,5 +4,6 @@ public class GithubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("fatih turker");
+        System.out.println("Yavuz");
     }
 }
